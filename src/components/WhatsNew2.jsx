9ee1622896ab22013012e124b2dee1123c42cf99
Maty1ek <124231838+Maty1ek@users.noreply.github.com>
2025-03-16 12:38:17 +0500

    fixed whatsnew 2

--- src/components/WhatsNew2.jsx
+++ src/components/WhatsNew2.jsx
@@ -2,7 +2,7 @@ const WhatsNew2 = ({ title, description, img, right }) => {
     return (
         <div className="whats_new_type_1 text-white flex flex-col md:flex-row items-center justify-between mb-16 sm:mb-20 md:mb-[130px] gap-8 px-4">
             <div
-                className={`whatsnew_card_img ${right ? "hidden md:block order-2" : "block order-1"} w-full sm:w-[80%] md:w-[470px]`}
+                className={`whatsnew_card_img ${right ? "hidden  " : "block "} w-full sm:w-[80%] md:w-[470px]`}
             >
                 <img src={`${img}`} alt="" className="rounded-[15px] w-full" />
             </div>
@@ -10,7 +10,7 @@ const WhatsNew2 = ({ title, description, img, right }) => {
                 <h1 className="text-3xl sm:text-4xl md:text-[55px] font-bold mb-4 sm:mb-[10px]">{title}</h1>
                 <p className="text-gray-300 mb-8 sm:mb-10 md:mb-[60px] text-sm sm:text-[15px]">{description}</p>
             </div>
-            <div className={`whatsnew_card_img ${right ? "block md:hidden" : "hidden"} w-full sm:w-[80%] order-2`}>
+            <div className={`whatsnew_card_img ${right ? "block " : "hidden"} w-full sm:w-[80%]  md:w-[470px]`}>
                 <img src={`${img}`} alt="" className="rounded-[15px] w-full" />
             </div>
         </div>
@@ -18,3 +18,4 @@ const WhatsNew2 = ({ title, description, img, right }) => {
 }
 
 export default WhatsNew2
+
